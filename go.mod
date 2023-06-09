d37bf6621bea648484fbd944656ac82a398e20d1
sunxiaoming <sunxiaoming@cmcm.com>
2023-06-09 18:26:19 +0800

    Initial go-main-project

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/sxiaoming2007/go-main-project
+
+go 1.18
+
+require github.com/sxiaoming2007/my-go-package-test v1.0.1 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/sxiaoming2007/my-go-package-test/myfunc"
+)
+
+func main() {
+	sum := myfunc.Myfunc(99, 1)
+	fmt.Println(sum)
+}
